894c84bcf630c71c10d1b0ffb6f9c070ede13e15
Basel Anani <baselanani@Basels-Air.MG8702>
2022-04-05 00:03:23 -0600

    Changed some CSS added PHP scripts

---
+++ signup.php
@@ -0,0 +1,13 @@
+<?php
+    $con = mysqli_connect('localhost', 'root', 'Emsrocks!!97', 'contracts');
+    $username = $_POST("['username']");
+    $password = $_POST("['password']");
+    
+    $sql = "INSERT INTO `business_user` (`username`, `password`) VALUES ('$username', '$password');";
+
+    $rs = mysqli_query($con, $sql);
+
+    if ($rs) {
+        echo "User added!"
+    }
+    ?>
